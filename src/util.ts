4f4e9ac63ff0cb4de3c009ec8fb5feb2f35b36b5
supreet-torinit <supreet.s.sethi@gmail.com>
2022-02-18 00:33:20 +0530

    task list movement management

--- src/util.ts
+++ src/util.ts
@@ -1,19 +1,69 @@
 import { StageAndTaskList, Task } from "./commonInterfaces";
 
-export const mapStageToTaskList = (stages:Array<string>) => {
-    const stageTaskListDict:StageAndTaskList = {};
-    stages.map((stage:string)=>{
-        stageTaskListDict[stage]=[];
-    });
-    return stageTaskListDict;
-}
+export const mapStageToTaskList = (stages: Array<string>) => {
+  const stageTaskListDict: StageAndTaskList = {};
+  stages.map((stage: string) => {
+    stageTaskListDict[stage] = [];
+  });
+  return stageTaskListDict;
+};
 
-
-export const addNewTaskToList=(stageTaskListDict:StageAndTaskList,newTask:Task) => {
-    const updatedStageTaskList = {...stageTaskListDict};
-    // assuming stageTaskListDict will have minimum one stage 
-    // assuming the new task would be added to the end of the first list
-    let firstStage = Object.keys(updatedStageTaskList)[0];
-    updatedStageTaskList[firstStage].push(newTask);
+export const addNewTaskToList = (
+  stageTaskListDict: StageAndTaskList,
+  newTask: Task
+) => {
+  const updatedStageTaskList = { ...stageTaskListDict };
+  // assuming stageTaskListDict will have minimum one stage
+  // assuming the new task would be added to the end of the first list
+  let firstStage = Object.keys(updatedStageTaskList)[0];
+  updatedStageTaskList[firstStage].push(newTask);
+  return updatedStageTaskList;
+};
+export const moveTaskToNextStage = (
+  stageTaskListDict: StageAndTaskList,
+  currentTaskIndex: number,
+  currentStage: string
+) => {
+  const updatedStageTaskList = { ...stageTaskListDict };
+  let currentTask = updatedStageTaskList[currentStage][currentTaskIndex];
+  let stageList = Object.keys(updatedStageTaskList);
+  let currentStageIndex = stageList.indexOf(currentStage);
+  if (currentStageIndex == stageList.length - 1)
+    return removeTask(stageTaskListDict, currentTaskIndex, currentStage);
+  else {
+    let nextStage = Object.keys(updatedStageTaskList)[currentStageIndex + 1];
+    updatedStageTaskList[currentStage].splice(currentTaskIndex, 1);
+    updatedStageTaskList[nextStage].unshift(currentTask);
     return updatedStageTaskList;
-}
+  }
+};
+
+export const moveTaskToPrevStage = (
+  stageTaskListDict: StageAndTaskList,
+  currentTaskIndex: number,
+  currentStage: string
+) => {
+  const updatedStageTaskList = { ...stageTaskListDict };
+  let currentTask = updatedStageTaskList[currentStage][currentTaskIndex];
+  let currentStageIndex = Object.keys(updatedStageTaskList).indexOf(
+    currentStage
+  );
+  if (currentStageIndex == 0)
+    return removeTask(stageTaskListDict, currentTaskIndex, currentStage);
+  else {
+    let prevStage = Object.keys(updatedStageTaskList)[currentStageIndex - 1];
+    updatedStageTaskList[currentStage].splice(currentTaskIndex, 1);
+    updatedStageTaskList[prevStage].push(currentTask);
+    return updatedStageTaskList;
+  }
+};
+
+export const removeTask = (
+  stageTaskListDict: StageAndTaskList,
+  currentTaskIndex: number,
+  currentStage: string
+) => {
+  const updatedStageTaskList = { ...stageTaskListDict };
+  updatedStageTaskList[currentStage].splice(currentTaskIndex, 1);
+  return updatedStageTaskList;
+};
